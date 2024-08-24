8de26e027a7ed118d32df2bc5e7509fc8278488b
aopmin <1433021114@qq.com>
2024-08-24 12:01:12 +0800

    修改deploy.sh部署脚本

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:aopmin/Aopmin-Notes master:gh-pages
+git push -f git@github.com:aopmin/Aopmin-Notes.git master:gh-pages
 
 cd -
